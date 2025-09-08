db6b3e3c118a854026036a123427e35f58c35ef3
Ojay95 <112874084+Ojay95@users.noreply.github.com>
2025-09-08 15:57:15 +0100

    project-setup

---
+++ src/main/java/org/notification_service/PrymoNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrymoNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrymoNotificationServiceApplication.class, args);
+    }
+
+}
